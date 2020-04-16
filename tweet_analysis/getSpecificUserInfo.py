33b633095a949cbcaa1270b7b1780df0bfa852c9
Shota <dummy@dummy.com>
2020-04-16 14:48:45 +0000

    twitter api py

---
+++ tweet_analysis/getSpecificUserInfo.py
@@ -0,0 +1,133 @@
+# coding:utf-8
+
+import tweepy
+from datetime import timedelta
+import sys
+from conf import twitterApi
+import csv
+import traceback
+import urllib.request
+import urllib.error
+
+CONSUMER_KEY = twitterApi.CONSUMER_KEY
+CONSUMER_SECRET = twitterApi.CONSUMER_SECRET
+ACCESS_KEY = twitterApi.ACCESS_KEY
+ACCESS_SECRET = twitterApi.ACCESS_SECRET
+
+CSV_EXTENSION = '.csv'
+MEDIA_DIR = '/appl/scripts/media/'
+
+'''
+get user's tweets from twitter and output to csv file
+'''
+
+
+class GetUserInfo():
+
+    def __init__(self):
+        self.num = 0  # 取得するツイートを計算する
+        # 全ツイートを入れる空のリストを用意
+        self.all_tweets = []
+
+    '''
+      get user's tweets from Twitter and output header to csv
+    '''
+
+    def get_tweets(self):
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        if len(sys.argv) < 2:
+            print('need one parameter')
+            return False
+        account = sys.argv[1]
+        # account = '@muni_gurume'
+        tweets_file_name = account + CSV_EXTENSION
+        # reply_file_name = account + '_reply' + CSV_EXTENSION
+        # media_file_name = account + '_media' + CSV_EXTENSION
+        print(account)
+        # tweet_mode='extended'を設定することで、tweetが切れないかつ、画像も取得できる。
+        tweets = api.user_timeline(account, count=200, tweet_mode='extended')  # ※3
+
+        # ファイルにツイートを出力。エクセルでの文字化け防止のためBOM付utf8で作成
+        with open(tweets_file_name, mode='w', encoding='utf_8_sig')as f:
+            tweets_writer = csv.writer(f)
+            # header
+            tweets_writer.writerow(
+                ['id_str', 'tweet_text', 'favorite', 'retweet_count', 'Data', 'in_reply_to_status_id_str', 'media_id',
+                 'media_url'])
+            self.output_tweet(tweets, tweets_writer)
+
+            # get every 200 tweets
+            while len(tweets) > 0:
+                # tweet_mode='extended'を設定することで、tweetが切れないかつ、画像も取得できる。
+                tweets = api.user_timeline(account, count=200, max_id=self.all_tweets[-1].id - 1,
+                                           tweet_mode='extended')
+                self.output_tweet(tweets, tweets_writer)
+                print(self.num, 'ツイート表示しました。')
+
+    '''
+        output tweets to csv file
+    '''
+
+    def output_tweet(self, tweets, tweets_writer):
+        self.all_tweets.extend(tweets)
+        for tweet in tweets:
+            tweet.created_at += timedelta(hours=9)
+
+            # replyを判別
+            # if tweet.in_reply_to_status_id is not None:
+            # print('this tweet is a reply')
+
+            # RTは除去
+            if tweet.full_text.startswith('RT'):
+                continue
+            else:
+
+                # 改行を除去
+                s = tweet.full_text.splitlines()
+                output_text = ''
+                for text_data in s:
+                    if text_data != '':
+                        output_text += text_data
+
+                # escape ' because syntax error occur when insert into DB
+                if "'" in output_text:
+                    print(output_text)
+                    output_text = output_text.replace("'", "''")
+                    print(output_text)
+
+                try:
+                    # output to tweets csv file
+                    media_url = tweet.entities['media'][0]['media_url_https']
+
+                    self.download_media(media_url)
+                    tweets_writer.writerow([tweet.id_str, output_text, str(tweet.favorite_count), str(tweet.retweet_count),
+                                           str(tweet.created_at), tweet.in_reply_to_status_id_str,
+                                           tweet.entities['media'][0]['id_str'],
+                                           str(media_url)])
+                except KeyError:
+                    # if a tweet doesn't have media , output tweet info except media
+                    tweets_writer.writerow(
+                        [tweet.id_str, output_text, str(tweet.favorite_count), str(tweet.retweet_count),
+                         str(tweet.created_at), tweet.in_reply_to_status_id_str])
+                self.num += 1
+    '''
+       
+    '''
+    def download_media(self, media_url):
+        pic_name = media_url.split('/')[-1]
+        dst_path = MEDIA_DIR + pic_name
+        try:
+            with urllib.request.urlopen(media_url) as web_file:
+                data = web_file.read()
+                with open(dst_path, mode='wb') as local_file:
+                    local_file.write(data)
+        except urllib.error.URLError as e:
+            print(e)
+            print(media_url)
+
+
+# execute
+if __name__ == '__main__':
+    GetUserInfo().get_tweets()
---
+++ tweet_analysis/insertTweets.py
@@ -0,0 +1,90 @@
+# coding:utf-8
+import psycopg2
+import csv
+import sys
+import re
+import traceback
+import logging
+
+'''
+  from csv file to db
+'''
+
+HOST = 'localhost'
+PORT = '5432'
+DB_NAME = 'gurume_db'
+USER_NAME = 'connect_user'
+DB_PASS = 'connect_user'
+
+# db name and sql
+INSERT_QUERY = 'insert into {0} values ({1})'
+TWEETS_TABLE_NAME = 'tweets (id,tweet_text,favorite,retweeted,tweet_created_date)'
+TWEETS_REPLY_TABLE = 'reply_tweets (id,in_reply_to_status_id,TWEET_TEXT,tweet_created_date)'
+MEDIA_TABLE_NAME = 'tweet_media(media_id,tweet_id,media_path,media_link)'
+
+# grume_db column number information
+ID_COLUMN_NUM = 0
+TEXT_COLUMN_NUM = 1
+FAVORITE_COLUMN_NUM = 2
+RETWEET_COLUMN_NUM = 3
+CREATEDATE_COLUMN_NUM = 4
+REPLY_STATUS_COLUMN_NUM = 5
+MEDIA_ID_COLUMN_NUM = 6
+MEDIA_LINK_COLUMN_NUM = 7
+
+MEDIA_DST_PATH = '/appl/scripts/media/'
+
+TWEETS_INSERT_QUERY = INSERT_QUERY.format(TWEETS_TABLE_NAME, 'id')
+print(TWEETS_INSERT_QUERY)
+
+
+class InsertTweets():
+    def __init__(self):
+        pass
+
+    def main(self):
+        # connect to db
+        connection = psycopg2.connect(
+            'host=' + HOST + ' port=' + PORT + ' dbname=' + DB_NAME + ' user=' + USER_NAME + ' password=' + DB_PASS)
+        cur = connection.cursor()
+
+        input_file_name = sys.argv[1] + '.csv'
+        # from csv file, create insert query
+        with open(input_file_name, 'r', encoding='utf-8')as f:
+            reader = csv.reader(f)
+            for index, line in enumerate(reader):
+                if index == 0:
+                    continue
+                print(line[5])
+                if line[REPLY_STATUS_COLUMN_NUM] != '':
+                    print('reply')
+                    insert_value = line[ID_COLUMN_NUM] + ',\'' + line[5] + '\',\'' + line[TEXT_COLUMN_NUM] + '\',\'' + line[
+                        CREATEDATE_COLUMN_NUM] +'\''
+                    print(INSERT_QUERY.format(TWEETS_REPLY_TABLE, insert_value))
+                    reply_insert_query = INSERT_QUERY.format(TWEETS_REPLY_TABLE, insert_value)
+                    cur.execute(reply_insert_query)
+                else:
+                    insert_value = line[ID_COLUMN_NUM] + ',\'' + line[TEXT_COLUMN_NUM] + '\',' + line[RETWEET_COLUMN_NUM] + ',' + \
+                                   line[FAVORITE_COLUMN_NUM] + ',\'' + line[CREATEDATE_COLUMN_NUM]+'\''
+                    print(line[TEXT_COLUMN_NUM])
+                    print(INSERT_QUERY.format(TWEETS_TABLE_NAME, insert_value))
+                    tweets_insert_query = INSERT_QUERY.format(TWEETS_TABLE_NAME, insert_value)
+                    cur.execute(tweets_insert_query)
+
+                # insert into media. if line doesn't have media info, skip this insert query
+                try:
+                    pic_name = line[MEDIA_LINK_COLUMN_NUM].split('/')[-1]
+                    local_media_path = MEDIA_DST_PATH + pic_name
+                    insert_value = line[MEDIA_ID_COLUMN_NUM] + ',' + line[ID_COLUMN_NUM] + ',\'' + local_media_path + '\',\'' + line[
+                        MEDIA_LINK_COLUMN_NUM] + '\''
+                    print(INSERT_QUERY.format(MEDIA_TABLE_NAME,insert_value))
+                    media_insert_query = INSERT_QUERY.format(MEDIA_TABLE_NAME,insert_value)
+                    cur.execute(media_insert_query)
+                except IndexError:
+                    continue
+        connection.commit()
+
+
+# execute
+if __name__ == '__main__':
+    InsertTweets().main()
